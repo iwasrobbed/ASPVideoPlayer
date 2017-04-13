7ef4741a0d78682dcb5e525c661917e40e395719
Rob Phillips <rphillips@viv.ai>
2017-04-12 17:41:26 -0700

    Only show hours if there are any to show

--- ASPVideoPlayer/Classes/ASPVideoPlayerControls.swift
+++ ASPVideoPlayer/Classes/ASPVideoPlayerControls.swift
@@ -367,7 +367,11 @@ open class ASPBasicControls: UIView, VideoPlayerControls, VideoPlayerSeekControl
 		let minutes = (totalSeconds / 60) % 60
 		let hours = totalSeconds / 3600
 		
-		return String(format: "%02d:%02d:%02d", hours, minutes, seconds)
+        if hours != 0 {
+            return String(format: "%02d:%02d:%02d", hours, minutes, seconds)
+        } else {
+            return String(format: "%02d:%02d", minutes, seconds)
+        }
 	}
 	
 	private func commonInit() {
